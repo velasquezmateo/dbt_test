3b7e0b151a9b0dd63297f5a11f8c987cebeb306a
velasquezmateo <mateov55@hotmail.com>
2026-03-30 18:40:29 -0500

    changes

--- first_project/models/embalses/staging/stg_embalses.sql
+++ first_project/models/embalses/staging/stg_embalses.sql
@@ -6,12 +6,13 @@ SELECT  id_rep,
 	cast(vol_util as FLOAT) as volumen_util,
 	cast(fecha_reg as DATE) as fecha_registro,
 	region
-FROM {{source('source','datos_embalses_crudos')}}
+FROM {{source('gcp_source','datos_embalses_crudos')}}
 ),
 
 stg_embalses as (
 SELECT *,
        {{ volumen('volumen_util') }} as estado_embalse
 FROM raw_data)
+
 
 SELECT * from stg_embalses
